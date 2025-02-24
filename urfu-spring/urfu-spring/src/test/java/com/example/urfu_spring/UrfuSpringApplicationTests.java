8e56b2ad2f3afa6213cbbe0e1f14de99d788afcc
TheikosAlexei <123018584+Mysashi@users.noreply.github.com>
2025-02-24 19:48:04 +0500

    Add files via upload
    Initial commit

---
+++ urfu-spring/urfu-spring/src/test/java/com/example/urfu_spring/UrfuSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.urfu_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrfuSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
