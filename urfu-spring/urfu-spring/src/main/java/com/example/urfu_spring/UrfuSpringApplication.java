21947292c420ac5068682d96ef2362613c7e7a42
Cheminov <misterfife@mail.ru>
2025-03-23 21:12:12 +0500

    Config

---
+++ urfu-spring/urfu-spring/src/main/java/com/example/urfu_spring/UrfuSpringApplication.java
@@ -0,0 +1,17 @@
+package com.example.urfu_spring;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootApplication
+public class UrfuSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UrfuSpringApplication.class, args);
+	}
+
+
+}
